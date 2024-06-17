a89c1ec1ca52a402441079ae63fe2c40846ddf90
guo shuo <3123750229@qq.com>
2024-06-17 16:18:27 +0000

    ADD

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# my_dart
+A sample command-line application with an entrypoint in `bin/`, library code
+in `lib/`, and example unit test in `test/`.
---
+++ bin/my_dart.dart
@@ -0,0 +1,5 @@
+import 'package:my_dart/my_dart.dart' as my_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${my_dart.calculate()}!');
+}
